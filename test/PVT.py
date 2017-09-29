70cf2ec34130f54c0ddcb9b6dc6e45b316c056e5
khabibullinra <khabibullinra@gmail.com>
2017-09-29 17:25:17 +0300

    Переделана структура PVT класса

--- test/PVT.py
+++ test/PVT.py
@@ -10,80 +10,64 @@ UniflocPy
 
 """
 
-
-class Fluid:
+class ComponentGeneral:
     """
-    базовый класс для описания свойств пластовых флюидов -
-    смеси воды, нефти и газа в соответствии с моделью нелетучей нефти black oil
-    позволяет рассчитать свойства флюидов для заданных термобарических условий
+    Абстрактный класс для описания компонентоы углеводородных флюидов
     """
-    def __init__(self, gamma_oil=0.86, gamma_gas =0.8, gamma_wat = 1, rsb_m3m3 = 100):
-        """
-        initialize fluids with default parameters
-        :param gamma_oil: optional oil specific gravity
-        :param gamma_gas: optional gas specific gravity
-        :param gamma_wat: optional water specific gravity
-        :param Rsb_m3m3: optional gas solution ratio
-        """
-        # зададим базовый набор свойств по умолчанию
-        self.GammaWater = gamma_wat     # water specific density
-        self.GammaOil = gamma_oil       # oil specific gravity
-        self.GammaGas = gamma_gas       # gas specific gravity
-        self.Rsb_m3m3 = rsb_m3m3        # gas solution ratio at bubble point
-        self.Fw = 0                     # water cut, fraction
+    def __init__(self):
+        self.gamma = 1          # specific gravity of component, dimensionless
+        self.rho_kgm3 = 1       # density with dimension
+        self.mu_cP = 1          # dynamic viscosity
+        """ термобарические условия """
+        self._p_bar = 1
+        self._t_c = 15
 
-        # calibration parameters
-        self._Pb_calibration_bar = 100    # bubble point calibration pressure
-        self._Pb_bar = 0                 # oil bubble point pressure if 0 or negative - calculated
-
-        self._Tb_C = 0                   # oil temperature for Pb_bar calc
-        self._Bo_m3m3 = 0
-        self._Muo_cP = 0
-        self._Mug_cP = 0
-        self._Muw_cP = 0
-        self._MuDeadOil_cP = 0           # dead oil viscosity calculated value
-
-        self._sigmaOil_Nm = 0            # oil surface tention
-        self._sigmaWater_Nm = 0          # water surface tention
-
-    """ bubble point pressure """
-    @property
-    def Pb_bar(self):
-        return self._Pb_bar  # здесь надо проверить если значение есть - вернуть, если нет то рассчитать
-
-    @Pb_bar.setter
-    def Pb_bar(self,value):     # set calibration
-        if value > 0:
-            self._Pb_calibration_bar = value
-        else
-            self._Pb_calibration_bar = value
-        self._Pb_bar = value
-
-    @property
-    def Pb_atm(self):
-        return self._Pb_bar * 1
-
-
-    def calcPVT(self, P_bar,T_C):
-        """
-        caculates all PVT properties according to correlation set selected and initial data given
-        :param P_bar: pressure for PVT calculations
-        :param T_C: temperature for PVT calculations
-        :return: fills PVT properties with correct values
-        """
+    def calc(self, P_atm, T_C):
+        """ recalculate all parameters according to some pressure and termperature"""
         return 1
 
+class GasGeneral(ComponentGeneral):
+    """
+    Класс для описания свойств углеводородных газов
+    """
+    def __init__(self):
+        super().__init__()
+        self._z = 0.9               # сверхсжимаемость
 
-class FluidStanding(Fluid):
-    """
-    class for PVT properties estimation based on Standing correlation set
-    """
-    pass
+    @property
+    def z(self):
+        return  self._z
 
 
-class FluidMcCain(FluidStanding):
+class OilGeneral(ComponentGeneral):
     """
-    class for PVT estimation based on McCain correlations
+    Класс для описания свойств нефти по модели нелетучей нефти
     """
-    pass
+    def __init__(self):
+        super().__init__()              # часть базовых свойств наследуется
+        self._gas = GasGeneral()        # create gas component
+        self.rsb_m3m3 = 100
+
+        self.pb_calibr_bar = 100        # калибровочное значение давления насыщения
+        self.tb_calibr_c = 50           # температуры для калибровки по давлению насыщения
+        self.bob_calibr_m3m3 = 1.2      # калибровочное значение объемного коэффициента
+        self.muob_calibr_cp = 1         # калибровочное значение вязкости при давлении насыщения
+
+        """ расчетные свойства """
+        self._rs_m3m3 = 1
+        self._bo_m3m3 = 1
+        self._mu_m3m3 = 1
+
+
+    @property
+    def gas(self):
+        return self._gas
+
+    def rs_m3m3(self):
+        """ газосодержание """
+        return self._rs_m3m3
+
+    def calc(self, P_atm, T_C):
+        """ реализация расчета свойств нефти """
+        self._rs_m3m3 = self.rsb_m3m3
 
--- test/unitconverter.py
+++ test/unitconverter.py
@@ -4,7 +4,69 @@
 import scipy.constants as const
 
 g = const.g   # gravity
-pi = const.pi 
+pi = const.pi
+
+class AbstractUnit():
+    """
+    Абстрактный класс для определения свойств размерной единицы измерения
+    Не надо создавать объекты этого класса в явнов виде
+    для опредения разрмерности надо задать словать _unit с названиями размерностей и переводных коэффциентов
+    """
+    def __init__(self, val):
+        self._value = val
+#        self._unit = {}
+       
+    def __getitem__(self, key):
+        # если значение или тип ключа некорректны, list выбросит исключение
+        return  self._value / self._unit[key]
+        
+    def __getattr__(self, name):
+        return  self._value /  self._unit[name] 
+    
+    def __str__(self):
+        return str(self._value) + ' atm'
+
+    def __setitem__(self, key, item):
+        self._value = item * self._unit[key] 
+        
+    def __setattr__(self, key, item):
+        if key in _unit:
+            print("device test")
+#        else:
+#            super(MyTest, self).__setattr__(name, value)
+            # in python3+ you can omit the arguments to super:
+            #super().__setattr__(name, value)
+
+class Pressure(AbstractUnit):
+    
+    def __init__(self, val):
+        super().__init__(val)
+        self._unit = {'Pa':1/const.bar, 'psi':const.psi/const.bar, 'bar':1, 'atm': const.atm/const.bar, 'MPa':1000000/const.bar}
+
+
+class Length(AbstractUnit):
+    
+    def __init__(self, val):
+        super().__init__(val)
+        self._unit = {'m':1, 'ft':const.foot, 'mm':1/1000, 'inch': const.inch, 'km':1000}
+
+    
+#    @property
+#    def atm(self):
+#        return self
+    
+#    @atm.setter
+#    def atm(self, value):
+#        return float.__new__(Pressure, value)
+    
+#    @property
+#    def psi(self):
+#        return  bar2psi(self._val)
+    
+#    @psi.setter
+#    def psi(self,value):
+#        self._val = psi2bar(value)
+    
 
 def psi2pa(value=1):
     return value * const.psi
@@ -12,6 +74,9 @@ def psi2pa(value=1):
 
 def bar2psi(value=1):
     return value * const.bar / const.psi
+
+def psi2bar(value=1):
+    return value / const.bar * const.psi
 
 
 def bar2atm(value=1):
