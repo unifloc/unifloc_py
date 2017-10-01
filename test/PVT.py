fcff14711e40e802d2012b73c46c42b1d66d853a
khabibullinra <khabibullinra@gmail.com>
2017-10-01 11:33:12 +0300

    попытка доработать PVT - расширить набор функций, которые долны поддеживаться

--- test/PVT.py
+++ test/PVT.py
@@ -16,7 +16,7 @@ class ComponentGeneral:
     Абстрактный класс для описания компонентоы углеводородных флюидов
     """
     def __init__(self):
-        self.gamma = 1          # specific gravity of component, dimensionless
+        self._gamma = 1          # specific gravity of component, dimensionless
         self.rho_kgm3 = 1       # density with dimension
         self.mu_cp = 1          # dynamic viscosity
         """ термобарические условия """
@@ -27,6 +27,14 @@ class ComponentGeneral:
         """ recalculate all parameters according to some pressure and termperature"""
         return 1
 
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @gamma.setter
+    def gamma(self, value):
+        self._gamma = value
+
 
 class GasGeneral(ComponentGeneral):
     """
@@ -35,13 +43,40 @@ class GasGeneral(ComponentGeneral):
     def __init__(self):
         super().__init__()
         self._z = 0.9               # сверхсжимаемость
+        self._pseudo_pressure_mpa = 1
+        self._pseudo_temperature_k = 1
+        self.gamma = 0.8
+
 
     @property
     def z(self):
         return  self._z
 
+    def _calc_z(self):
+        pass
 
-class OilGeneral(ComponentGeneral):
+    def _calc_bg(self):
+        pass
+
+    def _calc_mug(self):
+        pass
+
+    @ComponentGeneral.gamma.setter
+    def gamma(self, value):
+        self._gamma = value
+        self._pseudo_pressure_mpa = 4.9 - 0.4 * self._gamma
+        self._pseudo_temperature_k = 95 + 171 * self._gamma
+
+    @property
+    def pseudo_temperature_k(self):
+        return self._pseudo_temperature_k
+
+    @property
+    def pseudo_pressure_mpa(self):
+        return self._pseudo_pressure_mpa
+
+
+ class OilGeneral(ComponentGeneral):
     """
     Класс для описания свойств нефти по модели нелетучей нефти
     """
@@ -77,7 +112,42 @@ class OilGeneral(ComponentGeneral):
         else:
             return self.rsb_m3m3
 
+    def _calc_bo_m3m3(self, p_bar, t_c):
+        """
+        Расчет объемного коэффициента нефти по корреляции
+        :param p_bar:
+        :param t_c:
+        :return:
+        """
+        pass
+
     def calc(self, p_atm, t_c):
         """ реализация расчета свойств нефти """
         self._rs_m3m3 = self._calc_rs_m3m3(p_atm, t_c)
 
+
+class WaterGeneral(ComponentGeneral):
+    """
+    класс описывающий свойства воды
+    """
+    def __init__(self):
+        super().__init__()
+
+
+class Fluid:
+    """
+    класс описывающий флюид на основе модели нелетучей нефти
+    """
+    def __init__(self):
+        self._oil = OilGeneral()
+        self._water = ComponentGeneral()
+
+        self._qliq_m3day = 10   # liquid rate
+        self._fw = 0            # water cut, fraction
+
+    @property
+    def fw(self):
+        return self._fw
+
+    def calc_pvt(self, p_bar, t_c):
+        pass
--- test/PVTtest.py
+++ test/PVTtest.py
@@ -1,14 +1,22 @@
 import PVT
 import matplotlib.pyplot as plt
 import numpy as np
-a = PVT.OilGeneral()
-a.calc(230,59)
-print(a.rs_m3m3)
 
-pp = np.arange(1,300,20)
-rs=[]
-for p in pp:
-    a.calc(p,20)
-    rs.append(a.rs_m3m3)
-plt.plot(pp,rs)
-plt.show()
+def test2():
+    g = PVT.GasGeneral()
+    print(g.pseudo_pressure_mpa)
+
+def test1():
+    a = PVT.OilGeneral()
+    a.calc(230,59)
+    print(a.rs_m3m3)
+
+    pp = np.arange(1,300,20)
+    rs=[]
+    for p in pp:
+        a.calc(p,20)
+        rs.append(a.rs_m3m3)
+    plt.plot(pp,rs)
+    plt.show()
+
+test2()
