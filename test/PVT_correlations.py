30b37ae0856a86710fbe116250e0ce9612117295
AlexeyVodopyan <keshid@yandex.ru>
2018-07-10 17:35:54 +0300

    добавлен объемный коэффициент нефти по стендингу, плотность по Маккейну, корреляции по вязкости нефти, расчет псевдокритических давления и температуры с учетом влияния неув газов

--- test/PVT_correlations.py
+++ test/PVT_correlations.py
@@ -6,6 +6,8 @@ Created on Sat May  5 13:59:06 2018
 """
 import numpy as np
 
+# PVT свойства для нефти
+
 def unf_pb_Standing_MPaa(rsb_m3m3, gamma_oil=0.86, gamma_gas=0.6, t_K=350):
     """
     bubble point pressure calculation according to Standing (1947) correlation
@@ -242,7 +244,7 @@ def unf_FVF_VB_m3m3_above(bob, cofb_1MPa, pb_MPa, p_MPa):
     Oil Formation Volume Factor according equation for pressure above bubble point pressure
     ref1 book Mccain_w_d_spivey_j_p_lenn_c_p_petroleum_reservoir_fluid,third edition, 2011
     
-    ! Actually, this correlation is belonged ro Vasquez & Beggs (1980)
+    ! Actually, this correlation is belonged ro Vasquez & Beggs (1980). In some sources is noted that this is Standing correlation.
     ref2 Vazquez, M. and Beggs, H.D. 1980. Correlations for Fluid Physical Property Prediction. J Pet Technol 32 (6): 968-970. SPE-6719-PA
     cofb_1MPa - oil compressibility
     
@@ -275,4 +277,154 @@ def unf_compressibility_oil_VB_1Mpa(Rs_m3m3, t_K, gamma_oil,p_MPa, gamma_gas=0.6
     else:
         co_1MPa = 0
     return co_1MPa
+
+def unf_FVF_Standing_m3m3_saturated(Rs_m3m3, gamma_gas, gamma_oil, t_K):
+    """  
+    Oil Formation Volume Factor according Standing equation at bubble point pressure
+    ref1 Volumetric and phase behavior of oil field hydrocarbon systems / M.B. Standing Standing, M. B. 1981
+    
+    to be continued
+    
+    """ 
+    Rs_scfSTB = 5.61458333333333 * Rs_m3m3
+    t_F = 1.8*(t_K - 273.15) + 32
+    bo = 0.972 + 1.47*10**(-4)*(Rs_scfSTB*(gamma_gas/gamma_oil)**0.5 + 1.25*t_F)**(1.175)
+    return bo
+
+def unf_density_oil_McCain(p_MPa,pb_MPa,co_1MPa, Rs_m3m3, gamma_gas, t_K, gamma_oil,gamma_gasSP = 0):
+    """  
+    Oil density according Standing, M.B., 1977; Witte, T.W., Jr., 1987; and McCain, W.D., Jr. and Hill, N.C.,
+1995 correlation.
+    ref1 book Mccain_w_d_spivey_j_p_lenn_c_p_petroleum_reservoir_fluid,third edition, 2011
+    
+    to be continued
+    
+    """ 
+    Rs_scfSTB = 5.61458333333333 * Rs_m3m3
+    ro_po = 52.8 - 0.01 * Rs_scfSTB           # первое приближение
+    
+    if gamma_gasSP == 0:                      #если нет данных о плотности газа в сепараторе
+        gamma_gasSP = gamma_gas
+    epsilon = 0.000001                        
+    maxiter = 100                             # максимальное число иттераций
+    counter = 0
+    ro_po_current = ro_po
+    ro_po_previous = 0
+    while (abs(ro_po_current - ro_po_previous) > epsilon and counter < maxiter):
+        ro_po_previous = ro_po_current
+        ro_a = -49.8930 + 85.0149*gamma_gasSP - 3.70373*gamma_gasSP*ro_po + 0.0479818*gamma_gasSP*ro_po**2 + 2.98914 *ro_po - 0.0356888*ro_po**2
+        ro_po = (Rs_scfSTB * gamma_gas + 4600 * gamma_oil)/(73.71 + Rs_scfSTB*gamma_gas/ro_a)
+        ro_po_current = ro_po
+        counter = counter + 1
+    p_psia = p_MPa * 145.03773800721814
+    t_F = 1.8*(t_K - 273.15) + 32
+    if p_MPa < pb_MPa: 
+        dro_p = ( 0.167 + 16.181 * 10**(-0.0425*ro_po))*(p_psia/1000) - 0.01 * (0.299+263*10**(-0.0603*ro_po))*(p_psia/1000)**2
+        ro_bs = ro_po + dro_p
+        dro_t = (0.00302 +1.505*ro_bs**(-0.951))*(t_F-60)**0.938 - (0.0216 - 0.0233 * 10**(-0.0161*ro_bs))*(t_F-60)**0.475
+        ro_oR = ro_bs - dro_t
+    else:
+        pb_psia = p_MPa * 145.03773800721814
+        dro_p = ( 0.167 + 16.181 * 10**(-0.0425*ro_po))(pb_psia/1000) - 0.01 * (0.299+263*10**(-0.0603*ro_po))*(pb_psia/1000)**2
+        ro_bs = ro_po + dro_p
+        dro_t = (0.00302 +1.505*ro_bs**(-0.951))*(t_F-60)**0.938 - (0.0216 - 0.0233 * 10**(-0.0161*ro_bs))*(t_F-60)**0.475
+        ro_oRb = ro_bs - dro_t
+        co_1psi = 1/145.03773800722 * co_1MPa
+        ro_oR = ro_oRb * np.exp(co_1psi*(p_psia - pb_psia))
+    ro_oR = 1/0.0624279606 * ro_oR
+    return ro_oR
+"""
+def unf_density_oil_Standing
+    
+    return po
+    
+    позже допишу плотность, не получается найти источник
+"""
+"""
+def unf_deadoilviscosity_Standing(gamma_oil, t_K):
+    """  
+  #  Correlation for Oil viscosity according to Standing Correlation 
+    
+#    
+    
+""" 
+    не могу найти источник
+    API = 141.5 / gamma_oil - 131.5
+    viscosity__cP = (0.32 + 1.8 * (10 ** 7) / (API ** 4.53)) * \
+    (360 / (1.8 * (t__C + 273) - 260)) ** (10 ** (0.43 + 8.33 / (API)))
+    return viscosity__cP
+"""
+def unf_deadoilviscosity_Beggs_cP(gamma_oil, t_K):
+    API = 141.5/gamma_oil - 131.5
+    T_F = 1.8*(t_K - 273.15) + 32  
+    C = 10 ** (3.0324 - 0.02023 * API)/(T_F) ** (-1.163)
+    viscosity_cP = 10 ** C - 1
+    return viscosity_cP
+
+def unf_saturatedoilviscosity_Beggs_cP(deadoilviscosity_cP,Rs_m3m3):
+    # viscosity correlation for pressure below bubble point
+    Rs_scfSTB = 5.61458333333333 * Rs_m3m3
+    A=10.715*(Rs_scfSTB + 100)**(-0.515)
+    B=5.44*(Rs_scfSTB+150)**(-0.338)
+    viscosity_cP = A*deadoilviscosity_cP**B
+    return viscosity_cP
+
+def unf_undersaturatedoilviscosity_VB_cP(p_MPa,pb_MPa,bubblepointviscosity_cP):
+    # viscosity correlation for pressure above bubble point
+    # ref2 Vazquez, M. and Beggs, H.D. 1980. Correlations for Fluid Physical Property Prediction. J Pet Technol 32 (6): 968-970. SPE-6719-PA
+    p_psia = p_MPa * 145.03773800721814
+    pb_psia = pb_MPa * 145.03773800721814
+    m = 2.6 * p_psia**1.187*np.exp(-11.513 - 8.98*10**(-5)*p_psia)
+    viscosity_cP = bubblepointviscosity_cP * (p_psia/pb_psia)**m
+    return viscosity_cP
+ 
+def unf_undersaturatedoilviscosity_Petrovsky_cP(p_MPa,pb_MPa,bubblepointviscosity_cP):
+    # viscosity correlation for pressure above bubble point
+    # ref 1 Viscosity Correlations for Gulf of Mexico Crude Oils
+    A = -1.0146 + 1.3322*np.log(bubblepointviscosity_cP) - 0.4876*np.log(bubblepointviscosity_cP)**2 - 1.15036*np.log(bubblepointviscosity_cP)**3
+    p_psia = p_MPa * 145.03773800721814
+    pb_psia = pb_MPa * 145.03773800721814
+    viscosity_cP = bubblepointviscosity_cP + 1.3449 * 10**(-3)* (p_psia - pb_psia)*10**A
+    return viscosity_cP
+
+def unf_oil_viscosity_Beggs_VB_cP(deadoilviscosity_cP,Rs_m3m3,p_MPa,pb_MPa):
+    # function for calculating the viscosity at any pressure
+    saturatedviscosity_cP = unf_saturatedoilviscosity_Beggs_cP(deadoilviscosity_cP,Rs_m3m3)
+    if p_MPa<=pb_MPa:
+        viscosity_cP = saturatedviscosity_cP
+    else:
+        viscosity_cP = unf_undersaturatedoilviscosity_VB_cP(p_MPa, pb_MPa,saturatedviscosity_cP)
+    return viscosity_cP
+
+"""
+В дальнейшем функции для нефти будут дополняться, а пока перейдем к PVT для газа
+"""
+
+# PVT свойства для газа
+
+def unf_pseudocritical_properties_MPa_K(gamma_gas,Tc_H2S_K = 0,Tc_CO2_K = 0,Tc_N2_K = 0,pc_H2S_MPa = 1,pc_CO2_MPa = 1,pc_N2_MPa = 1,y_H2S = 0,y_CO2 = 0,y_N2 = 0):
+    """
+    correlation for pseudocritical properties
+    ref 1 Piper, L.D., McCain, W.D., Jr., and Corredor, J.H. “Compressibility Factors for 
+    Naturally Occurring Petroleum Gases.” Gas Reservoir Engineering. Reprint Series. Richardson, TX: SPE. Vol. 52 (1999) 186–200
+    """
+    if pc_H2S_MPa == 0 or pc_CO2_MPa == 0 or pc_N2_MPa == 0:
+        print('Please,enter the correct pressure values')
+        Tpc_K = 0
+        ppc_MPa = 0
+    else:
+        Tc_H2S_R = 1.8*(Tc_H2S_K - 273.15) + 491.67
+        Tc_CO2_R = 1.8*(Tc_CO2_K - 273.15) + 491.67
+        Tc_N2_R = 1.8*(Tc_N2_K - 273.15) + 491.67
+        pc_H2S_psia = pc_H2S_MPa * 145.03773800721814
+        pc_CO2_psia = pc_CO2_MPa * 145.03773800721814
+        pc_N2_psia = pc_N2_MPa * 145.03773800721814
+        J = 1.1582* 10**(-1) - 4.5820*10**(-1)*y_H2S *(Tc_H2S_R/pc_H2S_psia) - 9.0348*10**(-1)*y_CO2*(Tc_CO2_R/pc_CO2_psia) -6.6026 * 10**(-1) * y_N2*(Tc_N2_R/pc_N2_psia) + 7.0729 * 10**(-1)*gamma_gas - 9.9397 * 10**(-2)*gamma_gas**2
+        K = 3.8216 - 6.5340 * 10**(-2)*y_H2S *(Tc_H2S_R/pc_H2S_psia) - 4.2113 * 10**(-1)*y_CO2*(Tc_CO2_R/pc_CO2_psia) -9.1249 * 10**(-1)* y_N2*(Tc_N2_R/pc_N2_psia) + 1.7438 * 10 * gamma_gas -3.2191 * gamma_gas**2
+        Tpc_R = K**2/J
+        ppc_psia = Tpc_R / J
+        Tpc_K =  (Tpc_R - 491.67)/1.8 + 273.15
+        ppc_MPa = ppc_psia / 145.03773800721814
+    return ppc_MPa,Tpc_K
+
     
