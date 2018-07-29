d5b9eadf7331936b7b64635301b92ccc9dd2d000
Михаил Полешко <mishapoleshko2@mail.ru>
2018-07-29 22:06:57 +0300

    Добавлено расчет и описание Г-образной схемы замещения АД

--- test/motor_model/MotorBase.py
+++ test/motor_model/MotorBase.py
@@ -1,14 +1,17 @@
-import pandas as pd
 from pandas import read_csv
+import pandas as pd
+from os.path import dirname
 
 
 class Motor(object):
 
-    def __init__(self, motor_base, company, frequency_nom, d__mm):
+    def __init__(self, motor_base: pd.DataFrame.astype, company: str,
+                 frequency_nom: int, d__mm: int) -> None:
         self.motor_base = motor_base
         self.company = company
         self.frequency_nom = frequency_nom
         self.d__mm = d__mm
+        self.nominal_power_kW = None
 
     @property
     def current_coefficient(self):
@@ -32,17 +35,16 @@ class Motor(object):
 
 
 def get_coefficient(motor_base, company, frequency_nom, d__mm, parameter):
-    motor = motor_base.loc[motor_base['Company'] == company]\
+    current_motor = motor_base.loc[motor_base['Company'] == company]\
         .loc[motor_base['frequency_nom'] == frequency_nom]\
         .loc[motor_base['d_mm'] == d__mm]
 
     coefficients = []
     for i in range(7):
-        coefficient = (motor['{}_{}'.format(parameter, i)].values[0])
+        coefficient = (current_motor['{}_{}'.format(parameter, i)].values[0])
         coefficients.append(coefficient)
     return coefficients
 
 
 # TODO: сделать чтобы база уже в классе лежала
-motor_base1 = read_csv('/Users/poleshkomi/Desktop/Projects/Unifloc/test'
-                      '/Motor model/db/db_motor.csv', sep=';')
+motor_base =read_csv('{}/db/db_motor.csv'.format(dirname(__file__)), sep=';')
