5e2692bea4d9440b099d6d586ad8d4c7faa6ad03
Михаил Полешко <mishapoleshko2@mail.ru>
2018-08-15 00:16:47 +0300

    Добавлен расчет показателей ПЭД в зависимости от загрузки

--- test/motor_model/g_circuit.py
+++ test/motor_model/g_circuit.py
@@ -10,7 +10,7 @@ import numpy as np
 
 
 def calc_resistances(nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
-                     motor_lamda):
+                     motor_lamda, alfa_0 = 0.61, moments_division = 1.05):
 
     """
     Расчет сопротивлений Г-образной схемы замещения
@@ -24,10 +24,6 @@ def calc_resistances(nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
              r_1: активное сопротивление обмотки статора
              x_k: индуктивное сопротивление обмоток при неподвижном роторе
     """
-
-    #рекомендованное значение(отношение электромагнитного момента к вращающемуся)
-    moments_division = 1.05
-    alfa_0 = 0.61
 
     a = alfa_0/moments_division * (1 - nom_efficient) / nom_efficient * \
         (1 - nom_slip) / nom_slip - 1
@@ -45,8 +41,8 @@ def calc_resistances(nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
     return r_2, r_1, x_k
 
 
-def calc_idle(nom_voltage__V, nom_slip, nom_efficient, nom_cos,
-                         r_1__Om, r_2__Om, x_k__Om):
+def calc_idle(nom_voltage__V, nom_slip, nom_efficient, nom_cos, r_1__Om,
+              r_2__Om, x_k__Om):
     """
     Расчет режима холостого хода при номинальном напряжении
 
@@ -83,8 +79,8 @@ def calc_idle(nom_voltage__V, nom_slip, nom_efficient, nom_cos,
 
 
 def calc_g_circuit(slip, nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
-                   nom_cos, motor_lamda, frequency__Hz, voltage__V,
-                   r_1__Om, r_2__Om, x_k__Om):
+                   nom_cos, frequency__Hz, voltage__V, r_1__Om, r_2__Om,
+                   x_k__Om, moments_division=1.05):
     """
     Расчет механической характеристики ассинхроного двигателя
 
@@ -94,7 +90,6 @@ def calc_g_circuit(slip, nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
     :param nom_slip: номинальное скольжение АД
     :param nom_efficient: номинальный КПД АД
     :param nom_cos: номинальный коэффициент мощности АД
-    :param motor_lamda: отнощение максимального момента к номинальному
     :param frequency__Hz: рабочая частота вращения
     :param voltage__V: рабочее напряжение
     :param r_1__Om: активное сопротивление статора
@@ -104,11 +99,12 @@ def calc_g_circuit(slip, nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
             cos_fi - коэффициент мощности
             efficient - КПД АД
             I1__A - потребляемый ток
+            P_2__kW - развиваемая мощность статора
     """
 
     f_nom__Hz = frequency__Hz * (1 - nom_slip)
-    M_nom__Nm = nom_power__W / f_nom__Hz / 2 / math.pi
-    M0__Nm = M_nom__Nm * (motor_lamda - 1)
+    M_nom__Nm = nom_power__W / (f_nom__Hz * 2 * math.pi)
+    M0__Nm = M_nom__Nm * (moments_division - 1)
     M__Nm = 3 * voltage__V**2 * (r_2__Om / slip) /\
             (frequency__Hz * 2 * math.pi * ((r_1__Om + r_2__Om / slip)**2 +
                                             x_k__Om ** 2)) - M0__Nm
@@ -127,6 +123,50 @@ def calc_g_circuit(slip, nom_power__W, nom_voltage__V, nom_slip, nom_efficient,
     P_1__kW = 3 * voltage__V * I1__A * cos_fi / 1e3
     P_2__kW = M__Nm * frequency__Hz * 2 * math.pi / 1e3
     efficient = P_2__kW / P_1__kW
-    return M__Nm,  cos_fi, efficient, I1__A
+    return M__Nm,  cos_fi, efficient, I1__A, P_2__kW
 
 
+def motor_data_loading(motor_power__W, nom_power__W, nom_voltage__V, nom_slip,
+                       nom_efficient, nom_cos, frequency__Hz, voltage__V,
+                       r_1__Om, r_2__Om, x_k__Om, moments_division=1.05):
+    """
+
+    :param motor_power__W: рабочая мощность ПЭД
+    :param nom_power__W: номинальная мощность АД
+    :param nom_voltage__V: номинальное напряжение АД
+    :param nom_slip: номинальное скольжение АД
+    :param nom_efficient: номинальный КПД АД
+    :param nom_cos: номинальный коэффициент мощности АД
+    :param frequency__Hz: рабочая частота вращения
+    :param voltage__V: рабочее напряжение
+    :param r_1__Om: активное сопротивление статора
+    :param r_2__Om: активное сопротивление ротора приведенное к статору
+    :param x_k__Om: индуктивное сопротивление обмоток на режиме ХХ
+    :return:M__Nm - момент вращения ротора
+            cos_fi - коэффициент мощности
+            efficient - КПД АД
+            I1__A - потребляемый ток
+            slip - скольжение АД
+    """
+
+    def f(slip, args):
+        motor_power__W, nom_power__W, nom_voltage__V, nom_slip, nom_efficient, \
+        nom_cos, frequency__Hz, voltage__V, r_1__Om, r_2__Om, x_k__Om, \
+        moments_division = args
+
+        _, _, _, _, P_2__kW = calc_g_circuit(slip, nom_power__W, nom_voltage__V,
+                                             nom_slip, nom_efficient, nom_cos,
+                                             frequency__Hz, voltage__V, r_1__Om,
+                                             r_2__Om, x_k__Om, moments_division)
+        return P_2__kW - motor_power__W/1e3
+
+    slip = fsolve(func=f, args=[motor_power__W, nom_power__W, nom_voltage__V,
+                                nom_slip, nom_efficient, nom_cos, frequency__Hz,
+                                voltage__V, r_1__Om, r_2__Om, x_k__Om,
+                                moments_division], x0=nom_slip/2)
+
+    M__Nm, cos_fi, efficient, I1__A, P_2__kW = calc_g_circuit(
+        slip, nom_power__W, nom_voltage__V, nom_slip, nom_efficient, nom_cos,
+        frequency__Hz, voltage__V, r_1__Om, r_2__Om, x_k__Om, moments_division)
+
+    return M__Nm, cos_fi, efficient, I1__A, slip, P_2__kW
