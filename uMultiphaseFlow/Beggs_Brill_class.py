e4834e5db9fd633c02750eccadadb040860f146b
Oleg <oleg.kobzarius@gmail.com>
2019-06-02 01:12:47 +0300

    Согласован результат применения методики между расчетом в книге и реализованном расчете. Добавлено полное описание со всеми формулами. Пока неработающий пример с множеством графиков для отладки.

--- uMultiphaseFlow/Beggs_Brill_class.py
+++ uMultiphaseFlow/Beggs_Brill_class.py
@@ -10,7 +10,7 @@ import math
 
 const_g_m2sec = 9.8
 
-
+# TODO добавить учет расчета сверху вниз
 def __friction_coefficient__(Re, epsilon, d):
     # TODO исправить коэффициент
     """
@@ -39,23 +39,23 @@ def __calc_hltetta__(data):
     :return: ничего, последний расчет - объемное соодержание жидкости с поправкой на угол и поправкой Пэйна
     """
     if data.flow_regime == 0:
-        # Segregated Flow
+        # Segregated Flow - Расслоенный
         a = 0.98
         b = 0.4846
         c = 0.0868
     if data.flow_regime == 1:
-        # Intermittent Flow
+        # Intermittent Flow - Прерывистый
         a = 0.845
         b = 0.5351
         c = 0.0173
     if data.flow_regime == 2:
-        # Distributed Flow
+        # Distributed Flow - Распределенный
         a = 1.065
         b = 0.5824
         c = 0.0609
-    data.EL = a * data.liquid_content ** b / data.val_number_Fr ** c
+    data.hl0 = a * data.liquid_content ** b / data.val_number_Fr ** c
 
-    data.Nlv = (data.vsl_msec * (data.rho_liquid_kgm3 / (const_g_m2sec *  data.sigma_kgsec2)) ** (1 / 4))
+    data.Nlv = (data.vsl_msec * (data.rho_liquid_kgm3 / (const_g_m2sec * data.sigma_kgsec2)) ** (1 / 4))
 
     if data.flow_regime == 2:
         data.correction_factor_c = 0
@@ -79,10 +79,10 @@ def __calc_hltetta__(data):
             data.correction_factor_c = result
 
     data.angle_correction_factor = (1 + data.correction_factor_c *
-                                    ((math.sin(1.8 * data.angle_grad)) - (1/3) *
-                                    (math.sin(1/8 * data.angle_grad))**3))
+                                    ((math.sin(1.8 * data.angle_rad)) - (1/3) *
+                                    (math.sin(1.8 * data.angle_rad))**3))
 
-    data.volume_liquid_content_with_angle = data.EL * data.angle_correction_factor
+    data.volume_liquid_content_with_angle = data.hl0 * data.angle_correction_factor
 
     if data.angle_grad > 0:
         data.volume_liquid_content_with_Pains_cor = 0.924 * data.volume_liquid_content_with_angle
@@ -109,17 +109,18 @@ class Beggs_Brill_cor():
         self.water_rate_on_surface_m3day = 0
 
         self.Rp_m3m3 = 178  # газовый фактор
-        self.Rs_m3m3 = 50.6
-        self.Rsw_m3m3 = 0
+        self.rs_m3m3 = 50.6
+        self.rsw_m3m3 = 0
         self.rho_oil_kgm3 = 762.64
         self.rho_water_kgm3 = 1000
         self.rho_liquid_kgm3 = 762.64
         self.rho_gas_kgm3 = 94.19
-        self.oil_formation_volume_factor_m3m3 = 1.197
-        self.gas_formation_volume_factor_m3m3 = 0.0091
+        self.bo_m3m3 = 1.197
+        self.bw_m3m3 = 1
+        self.bg_m3m3 = 0.0091
 
         self.angle_grad = 90  # угол наклона ствола скважины от горизонтали
-
+        self.angle_rad = self.angle_grad * math.pi / 180
         self.Ap = None  # поперечная площадь трубы
 
         self.volume_oil_rate_in_condition_m3sec = 0  # объемный дебит нефти при данных условиях (P,T)
@@ -139,7 +140,7 @@ class Beggs_Brill_cor():
 
         self.vsl_msec = 0  # приведенная скорость жидкости (3.10)
         self.vsg_msec = 0  # приведенная скорость газа (3.11)
-        self.vsm_msec = 0  # приведенная (общая) скорость смеси (3.12)
+        self.vm_msec = 0  # приведенная (общая) скорость смеси (3.12)
 
         self.liquid_content = 0  # объемное содержание жидкости при отсутствии проскальзывания (3.8)
 
@@ -156,7 +157,7 @@ class Beggs_Brill_cor():
         self.Ek = None
         self.rhos_kgm3 = None
         self.volume_liquid_content_with_Pains_cor = None
-        self.grad = None
+        self.grad_pam = None
         self.print_all = True
 
     def calc_grad(self, PT):
@@ -168,30 +169,32 @@ class Beggs_Brill_cor():
         self.pressure_pa = PT.p_pa
         self.temperature_c = PT.T_C
         if self.pressure_pa <= 0:
-            self.grad = 0
+            self.grad_pam = 0
             return 0
         else:
 
             self.Ap = math.pi * self.diametr_inner_m ** 2 / 4  # площадь поперечного сечения трубы, м2
 
-            self.volume_oil_rate_in_condition_m3sec = self.oil_rate_on_surface_m3day * self.oil_formation_volume_factor_m3m3 * 0.9998 / 86400  # (3.1)
+            self.volume_oil_rate_in_condition_m3sec = self.oil_rate_on_surface_m3day * self.bo_m3m3 / 86400  # (3.1)
+
+            self.volume_water_rate_in_condition_m3sec = self.water_rate_on_surface_m3day * self.bw_m3m3 / 86400
 
             self.volume_liquid_rate_in_condition_m3sec = self.volume_oil_rate_in_condition_m3sec + self.volume_water_rate_in_condition_m3sec
 
             self.vsl_msec = self.volume_liquid_rate_in_condition_m3sec / self.Ap  # приведенная скорость жидкости (3.10)
 
             self.volume_gas_rate_in_condition_m3sec = (self.gas_rate_on_surface_m3day -
-                                                       self.oil_rate_on_surface_m3day * self.Rs_m3m3 -
-                                                       self.water_rate_on_surface_m3day * self.Rsw_m3m3) * self.gas_formation_volume_factor_m3m3 / 86400  # (3.3)
+                                                       self.oil_rate_on_surface_m3day * self.rs_m3m3 -
+                                                       self.water_rate_on_surface_m3day * self.rsw_m3m3) * self.bg_m3m3 / 86400  # (3.3)
 
             self.vsg_msec = self.volume_gas_rate_in_condition_m3sec / self.Ap  # приведенная скорость газа (3.11)
 
-            self.vsm_msec = self.vsl_msec + self.vsg_msec  # приведенная скорость смеси
+            self.vm_msec = self.vsl_msec + self.vsg_msec  # приведенная скорость смеси
 
             self.liquid_content = self.volume_liquid_rate_in_condition_m3sec / (
                     self.volume_liquid_rate_in_condition_m3sec + self.volume_gas_rate_in_condition_m3sec)  # содержание жидкости в потоке
 
-            self.val_number_Fr = self.vsm_msec ** 2 / const_g_m2sec / self.diametr_inner_m  # (4.109)
+            self.val_number_Fr = self.vm_msec ** 2 / const_g_m2sec / self.diametr_inner_m  # (4.109)
 
             number_Fr = self.val_number_Fr
 
@@ -222,9 +225,6 @@ class Beggs_Brill_cor():
                 self.flow_regime = 1
                 __calc_hltetta__(self)
                 hltetta_inter = self.volume_liquid_content_with_Pains_cor
-                L2 = 0.0009252 * self.liquid_content ** (-2.4684)
-                L3 = 0.1 * self.liquid_content ** (-1.4516)
-
                 A = (L3 - self.val_number_Fr) / (L3 - L2)
                 B = 1 - A
                 self.volume_liquid_content_with_Pains_cor = (A * hltetta_segr + B * hltetta_inter)
@@ -240,20 +240,10 @@ class Beggs_Brill_cor():
 
             self.rhon_kgm3 = self.rho_liquid_kgm3 * self.liquid_content + self.rho_gas_kgm3 * (1 - self.liquid_content)
 
-            self.number_Re = self.rhon_kgm3 * self.vsm_msec * self.diametr_inner_m / self.mu_mix_noslip_pas
+            self.number_Re = self.rhon_kgm3 * self.vm_msec * self.diametr_inner_m / self.mu_mix_noslip_pas
 
             self.friction_coefficient = __friction_coefficient__(self.number_Re, self.epsilon_friction_m,
                                                                  self.diametr_inner_m)
-            if self.friction_coefficient == None:
-                print(self.pressure_pa)
-                print('lol')
-                print(self.rhon_kgm3)
-                print(self.vsm_msec)
-                print(self.mu_mix_noslip_pas)
-                print(self.number_Re)
-                print(self.epsilon_friction_m)
-                print(self.diametr_inner_m)
-            #    self.friction_coefficient = 0.01
 
             self.y = self.liquid_content / self.volume_liquid_content_with_Pains_cor ** 2
             if 1 < self.y < 1.2:
@@ -266,13 +256,13 @@ class Beggs_Brill_cor():
 
             self.result_friction = self.friction_coefficient * math.exp(self.s)
 
-            self.Ek = self.vsm_msec * self.vsg_msec * self.rhon_kgm3 / self.pressure_pa
+            self.Ek = self.vm_msec * self.vsg_msec * self.rhon_kgm3 / self.pressure_pa
 
             self.rhos_kgm3 = (self.rho_liquid_kgm3 * self.volume_liquid_content_with_Pains_cor +
                              self.rho_gas_kgm3 * (1 - self.volume_liquid_content_with_Pains_cor))
 
-            self.grad = (self.result_friction * self.rhon_kgm3 * self.vsm_msec ** 2 / 2 / self.diametr_inner_m +
-                         self.rhos_kgm3 * const_g_m2sec * math.sin(self.angle_grad) / ( 1 - self.Ek))
+            self.grad_pam = (self.result_friction * self.rhon_kgm3 * self.vm_msec ** 2 / 2 / self.diametr_inner_m +
+                             self.rhos_kgm3 * const_g_m2sec * math.sin(self.angle_rad) / ( 1 - self.Ek))
 
             if self.print_all:
                 __out__('Ap', self.Ap)
@@ -281,7 +271,7 @@ class Beggs_Brill_cor():
                 __out__('vsl_msec', self.vsl_msec)
                 __out__('volume_gas_rate_in_condition_m3sec', self.volume_gas_rate_in_condition_m3sec)
                 __out__('vsg_msec', self.vsg_msec)
-                __out__('vsm_msec', self.vsm_msec)
+                __out__('vsm_msec', self.vm_msec)
                 __out__('liquid_content', self.liquid_content)
                 __out__('val_number_Fr', self.val_number_Fr)
                 __out__('flow_regime', self.flow_regime)
@@ -300,9 +290,9 @@ class Beggs_Brill_cor():
                 __out__('result_friction', self.result_friction)
                 __out__('Ek', self.Ek)
                 __out__('rhos_kgm3', self.rhos_kgm3)
-                __out__('grad_barm', self.grad / 10 ** 5)
+                __out__('grad_barm', self.grad_pam / 10 ** 5)
 
-            return self.grad
+            return self.grad_pam
 
 class PT():
     def __init__(self, p_mpa, t_c):
@@ -317,5 +307,5 @@ class_example = Beggs_Brill_cor()
 class_example.print_all = False
 result_grad = class_example.calc_grad(PT_example)
 print(result_grad / 10**5)
-print(class_example.grad / 10**5)
+print(class_example.grad_pam / 10 ** 5)
 
