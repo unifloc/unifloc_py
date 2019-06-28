f7ddf0a8b753bd73e5909ae3c17c558f864a2efc
Oleg <oleg.kobzarius@gmail.com>
2019-06-28 23:53:43 +0300

    Добавление модуля по определению режима потока в затрубном пространстве

--- uniflocpy/uMultiphaseFlow/flow_pattern_annulus_Caetano.py
+++ uniflocpy/uMultiphaseFlow/flow_pattern_annulus_Caetano.py
@@ -11,7 +11,6 @@ class flow_pattern_annulus_Caetano(object):
         self.rho_liq_kgm3 = 800
         self.rho_gas_kgm3 = 50
 
-        self.gas_void_fraction = 0.2
         self.d_cas_in_m = 0.155
         self.d_tube_out_m = 0.80
 
@@ -27,11 +26,11 @@ class flow_pattern_annulus_Caetano(object):
         # рассчитанные параметры
         self.equation_part = None
         self.v_infinite_z_msec = None
-
+        self.v_Taylor_bubble_msec = None
         self.d_equi_periphery_m = None
         self.d_hydr_m = None
 
-        self.flow_pattern_transition = None
+        self.flow_pattern = None
 
         self.v_slip_m3sec = None
 
@@ -39,7 +38,7 @@ class flow_pattern_annulus_Caetano(object):
 
         self.friction_coeff = None
 
-        self.d_max__bubble_m = None
+        self.d_max_bubble_m = None
 
         self.d_crit_bubble_m = None
 
@@ -49,10 +48,14 @@ class flow_pattern_annulus_Caetano(object):
 
         self.Fca = None
 
+        self.vs_gas_bubble2slug_msec = None
+        self.vs_gas_dispbubble2slug_msec = None
+        self.vs_gas_2annular_msec = None
+
     def __friction_coefficient_Gunn_Darling(self, initial_f):
-        right_part = 4 * math.log(self.number_Re * (initial_f *
+        right_part = (4 * math.log(self.number_Re * (initial_f *
                                                 (16 / self.Fca) ** (0.45 * math.exp(-(self.number_Re - 3000) / 10 ** 6))
-                                                ) ** 0.5) - 0.4
+                                                ) ** 0.5) - 0.4)
         left_part = 1 / (initial_f * (16 / self.Fca) ** (0.45 * math.exp(-(self.number_Re - 3000) / 10 ** 6))) ** 0.5
 
         return right_part - left_part
@@ -69,57 +72,90 @@ class flow_pattern_annulus_Caetano(object):
         # Nicklin (1962)
         self.v_Taylor_bubble_msec = 0.35 * (uc.g * self.d_equi_periphery_m) ** 0.5
         if self.v_Taylor_bubble_msec >= self.v_infinite_z_msec:
-            self.flow_pattern_transition = 0
+            self.flow_pattern = 0  # bubble flow pattern
 
         # Bubble to Slug Flow Transition
-        self.v_slip_m3sec = (self.vs_gas_msec / self.gas_void_fraction -
-                             self.vs_liq_msec / (1 - self.gas_void_fraction))
-        # transition between bubbly to slug flow
-        self.vs_gas_msec = (self.gas_void_fraction / (1 - self.gas_void_fraction) * self.vs_liq_msec +
-                            1.53 * self.gas_void_fraction * self.equation_part)
         if self.concentric_annulus:
-            self.vs_gas_msec = self.vs_liq_msec / 4 + 0.306 * self.equation_part
+            self.vs_gas_bubble2slug_msec = self.vs_liq_msec / 4 + 0.306 * self.equation_part
         else:
-            self.vs_gas_msec = self.vs_liq_msec / 5.67 + 0.230 * self.equation_part
+            self.vs_gas_bubble2slug_msec = self.vs_liq_msec / 5.67 + 0.230 * self.equation_part
 
         # bubble or slug to dispersed bubble flow transition
         # Calderbank (1958)
         self.const_k = 0.725 + 4.15 * (self.vs_gas_msec / self.vm_msec) ** 0.5
-        self.d_hydr_m = self.d_tube_out_m - self.d_tube_out_m
-        # Hinze (1955)
-        self.d_max__bubble_m = (self.const_k * (self.surface_tension_gl_Nm / self.rho_liq_kgm3) ** (0.6) *
-                        (2 * self.vm_msec ** 3 / self.d_hydr_m * self.friction_coeff) ** (-0.4))
+        self.d_hydr_m = self.d_cas_in_m - self.d_tube_out_m
         # Broodkey (1967)
-        self.d_crit_bubble_m = (0.4 * self.surface_tension_gl_Nm / (self.rho_liq_kgm3 - self.rho_gas_kgm3) / uc.g) ** 0.5
-        if self.d_crit_bubble_m >= self.d_max__bubble_m:
-            self.flow_pattern_transition = 1
-
         self.number_Re = self.rho_mix_kgm3 * self.vm_msec * self.d_hydr_m / self.mu_mix_pasec
-
         self.k_ratio_d = self.d_tube_out_m / self.d_cas_in_m
-
         # Friction coefficient
         # TODO дописать формулы для эксентричного положения трубы (сложные какие-то) - или не надо?
+        self.Fca = (16 * (1 - self.k_ratio_d) ** 2 /
+                    ((1 - self.k_ratio_d ** 4) / (1 - self.k_ratio_d ** 2) -
+                     (1 - self.k_ratio_d ** 2) / math.log(1 / self.k_ratio_d)))
         if self.number_Re < 3000:  # laminar flow
             if self.concentric_annulus:
-                self.Fca = (16 * (1 - self.k_ratio_d) ** 2 /
-                                       ((1 - self.k_ratio_d ** 4) / (1 - self.k_ratio_d ** 2) -
-                                        (1 - self.k_ratio_d ** 2) / math.log(1 / self.k_ratio_d)))
                 self.friction_coeff = self.Fca / self.number_Re
-            else:
-                self.friction_coeff = (1 / self.number_Re * 4 * (1 - self.k_ratio_d) ** 2 * (1 - self.k_ratio_d ** 2) /
-                                       self.o1o / math.sinh(self.eno) ** 4)
+            #else:
+            #    self.friction_coeff = (1 / self.number_Re * 4 * (1 - self.k_ratio_d) ** 2 * (1 - self.k_ratio_d ** 2) /
+            #                           self.o1o / math.sinh(self.eno) ** 4)
         else:  # turbulent flow
             if self.concentric_annulus:
                 self.friction_coeff = float(sp.fsolve(self.__friction_coefficient_Gunn_Darling, 0.000005))
+        # Hinze (1955)
+        self.d_max_bubble_m = (self.const_k * (self.surface_tension_gl_Nm / self.rho_liq_kgm3) ** (0.6) *
+                               (2 * self.vm_msec ** 3 / self.d_hydr_m * self.friction_coeff) ** (-0.4))
+
+        self.d_crit_bubble_m = (0.4 * self.surface_tension_gl_Nm / (self.rho_liq_kgm3 -
+                                                                    self.rho_gas_kgm3) / uc.g) ** 0.5
 
         # Dispersed bubble to slug flow transition
-
-        self.vs_gas_msec = 1.083 * self.vs_liq_msec + 0.796 * self.equation_part
+        self.vs_gas_dispbubble2slug_msec = 1.083 * self.vs_liq_msec + 0.796 * self.equation_part
 
         # Transition to Annular flow
+        self.vs_gas_2annular_msec = 3.1 * self.equation_part
 
-        self.vs_gas_msec = 3.1 * self.equation_part
+        #  определение режима потока
+        if self.d_crit_bubble_m >= self.d_max_bubble_m:
+            self.flow_pattern = 1  # dispersed bubble flow pattern
+        else:
+            if self.vs_gas_msec < self.vs_gas_bubble2slug_msec:
+                self.flow_pattern = 0  # bubble flow pattern
+            else:
+                if self.vs_gas_msec > self.vs_gas_2annular_msec:
+                    self.flow_pattern = 3  # annular flow
+                else:
+                    if self.vs_gas_msec < self.vs_gas_dispbubble2slug_msec:
+                        self.flow_pattern = 1  # dispersed bubble flow pattern
+                    else:
+                        self.flow_pattern = 3  # slug flow pattern
+
+annulus = flow_pattern_annulus_Caetano()
+annulus.calc_pattern()
+
+
+import uniflocpy.uPVT.PVT_fluids as PVT
+
+annular = flow_pattern_annulus_Caetano()
+fluid_flow = PVT.FluidFlow()
+
+p_bar = 40
+t_c = 60
+
+fluid_flow.calc(p_bar, t_c)
+
+annular.surface_tension_gl_Nm = fluid_flow.sigma_liq_Nm
+annular.rho_liq_kgm3 = fluid_flow.rho_liq_kgm3
+annular.rho_gas_kgm3 = fluid_flow.fl.rho_gas_kgm3
+annular.rho_mix_kgm3 = fluid_flow.rhon_kgm3
+annular.mu_mix_pasec = fluid_flow.mun_cP / 10 ** 3
 
 
 
+
+annular.d_cas_in_m = 0.150
+annular.d_tube_out_m = 0.100
+
+annular.vs_gas_msec = fluid_flow.vsg_msec
+annular.vs_liq_msec = fluid_flow.vsl_msec
+
+annular.calc_pattern()
