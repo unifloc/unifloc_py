e4474f7d00ab66c238be58adc649f1bdaa1057cb
kobzarius <oleg.kobzarius@gmail.com>
2019-12-16 14:19:27 +0300

    Измение типов для report в plotly_option.py. Добавление функции combine_multiprocessing_result в preproc_tool.py для объединение результатов многопоточного расчета

--- sandbox/uTools/preproc_p/preproc_tool.py
+++ sandbox/uTools/preproc_p/preproc_tool.py
@@ -1,1 +1,22 @@
+import os
+import pandas as pd
 
+
+def combine_multiprocessing_result(path_to_work_dir, dir_name_with_calculated_data):
+    filenames_list = []
+    for (dirpath, dirnames, filenames) in os.walk(path_to_work_dir + dir_name_with_calculated_data + 'multiprocessing\\'):
+        filenames_list.extend(filenames)
+        break
+    print(filenames_list)
+    for i, j in enumerate(filenames_list):
+        if i == 0:
+            first_result_data = pd.read_csv(path_to_work_dir + dir_name_with_calculated_data + 'multiprocessing\\' + j,
+                                            parse_dates=True, index_col='Время')
+        else:
+            another_result_data = pd.read_csv(path_to_work_dir + dir_name_with_calculated_data + 'multiprocessing\\' + j,
+                                              parse_dates=True, index_col='Время')
+            first_result_data = first_result_data.append(another_result_data, sort=True)
+            del first_result_data['d']
+            first_result_data = first_result_data.dropna(subset=['ESP.ESPpump.EffiencyESP_d'])
+    first_result_data = first_result_data.sort_index()
+    return first_result_data
