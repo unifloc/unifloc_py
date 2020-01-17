8cc6476a093904b77ced3a2f900917f0e01ea2ec
kobzarius <oleg.kobzarius@gmail.com>
2020-01-17 22:27:37 +0300

    Добавление возможности чтение данных о скважины из двух форматов (града и базы) в workflow_tr_data.py

--- sandbox/uTools/preproc_p/workflow_tr_data.py
+++ sandbox/uTools/preproc_p/workflow_tr_data.py
@@ -29,12 +29,34 @@ def extract_power_from_motor_name(name_str):
     return float(name_str)
 
 
+def to_float(string):
+    try:
+        string = string.replace(',', '.')
+        string = string.replace(' ', '')
+        this_float = float(string)
+    except:
+        this_float = float(string)
+    return this_float
+
+
 class Tr_data:
-    def __init__(self, row):
+    def __init__(self):
         """
         Класс-структура для хранения данных о скважине из техрежима
         :param row: строка, извлеченная с техрежима для данной скважины
         """
+        self.d_cas_mm = None
+        self.d_tube_mm = None
+        self.esp_nom_rate_m3day = None
+        self.esp_nom_head_m = None
+        self.h_pump_m = None
+        self.esp_name_str = None
+        self.udl_m = None
+        self.i_motor_nom_a = None
+        self.motor_name_str = None
+        self.power_motor_nom_kwt = None
+
+    def fill_by_true_tr(self, row):
         self.d_cas_mm = row[('D э/к', 'Unnamed: 9_level_1', 'Unnamed: 9_level_2', 'мм')].values[0]
         self.d_tube_mm = row[('D нкт', 'Unnamed: 10_level_1', 'Unnamed: 10_level_2', 'мм')].values[0]
         self.esp_nom_rate_m3day = \
@@ -49,16 +71,59 @@ class Tr_data:
         self.power_motor_nom_kwt = extract_power_from_motor_name(
             row[('ПЭД', 'Unnamed: 128_level_1', 'Марка', 'Unnamed: 128_level_3')].values[0])
 
+    def fill_by_grad(self, this_well_row):
+        self.d_cas_mm = to_float(this_well_row['Диаметр экспл.колонны'].values[0])
+        self.d_tube_mm = to_float(this_well_row['Диаметр НКТ'].values[0])
+        self.esp_nom_rate_m3day = to_float(this_well_row['Производительность ЭЦН'].values[0])
+        self.esp_nom_head_m = to_float(this_well_row['Напор'].values[0])
+        self.h_pump_m = to_float(this_well_row['Глубина спуска'].values[0])
+        self.esp_name_str = this_well_row['Тип насоса'].values[0]
+        self.udl_m = to_float(this_well_row['Глубина спуска'].values[0]) / \
+                     to_float(this_well_row['Глубина верхних дыр перфорации'].values[0]) * \
+                     to_float(this_well_row['Удлинение'].values[0])
+        self.i_motor_nom_a = to_float(this_well_row['Ток номинальный'].values[0])
+        if self.i_motor_nom_a > 0:
+            pass
+        else:
+            self.i_motor_nom_a = to_float(this_well_row['Ток рабочий'].values[0]) * 1.5
+            print('Нет значения номинального тока двитателя!')
+            if self.i_motor_nom_a > 0:
+                pass
+            else:
+                self.i_motor_nom_a = 30
+                print('Нет значения рабочего тока двитателя!')
 
-def read_tr_and_get_data(tr_file_full_path, well_name):
+    def fill_by_pump_base(self, this_well_row):
+        self.motor_name_str = this_well_row['Длина'].values[0]
+        self.power_motor_nom_kwt = extract_power_from_motor_name(self.motor_name_str)
+
+def read_tr_and_get_data(tr_file_full_path, well_name,
+                         pump_base_path='БОМД. Оборудование актуальных паспортов_Филиал Муравленковскнефть ОАО Газпромнефть-ННГ_30.12.2019.xlsx',
+                         field='Вынгаяхинское'):
     """
     Чтение техрежима и извлечение данных по скважине
     :param tr_file_full_path: абсолютный путь техрежиму
     :param well_name: номер скважины, str
     :return: класс-структура со всем данными
     """
-    tr = pd.read_excel(tr_file_full_path, skiprows=6,
-                       header=[0, 1, 2, 3])  # при ошибке файл нужно открыть и сохранить повторно без изменений
-    this_well_row = tr[tr[('№\nскв', 'Unnamed: 4_level_1', 'Unnamed: 4_level_2', 'Unnamed: 4_level_3')] == well_name]
-    tr_data = Tr_data(this_well_row)
+    try:
+        tr = pd.read_excel(tr_file_full_path, skiprows=6,
+                           header=[0, 1, 2, 3])  # при ошибке файл нужно открыть и сохранить повторно без изменений
+        this_well_row = tr[
+            tr[('№\nскв', 'Unnamed: 4_level_1', 'Unnamed: 4_level_2', 'Unnamed: 4_level_3')] == well_name]
+        tr_data = Tr_data()
+        tr_data.fill_by_true_tr(this_well_row)
+    except:
+        tr = pd.read_csv(tr_file_full_path, engine='c', delimiter=';', skiprows=2)
+        this_well_row = tr[tr['Скважина'] == well_name]
+        if this_well_row.shape[0] == 0:
+            this_well_row = tr[tr['Скважина'] == well_name + '_1']
+        if this_well_row.shape[0] == 2:
+            this_well_row = this_well_row[this_well_row.index == this_well_row.index[0]]
+        tr_data = Tr_data()
+        tr_data.fill_by_grad(this_well_row)
+
+        pump_base = pd.read_excel(pump_base_path, skiprows=3)
+        this_well_row = pump_base[(pump_base['№ скважины'] == well_name) & (pump_base['Месторождение'] == field)]
+        tr_data.fill_by_pump_base(this_well_row)
     return tr_data
