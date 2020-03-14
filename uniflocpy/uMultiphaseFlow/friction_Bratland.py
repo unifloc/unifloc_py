a708b97bf4a0129c72ce285d24f47284e6486295
kobzarius <oleg.kobzarius@gmail.com>
2020-03-14 13:33:23 +0300

    Устранение бага с трением по Ove Bratland. Работает scipy.root с методом excitingmixing, иначе ломается на Mac. Начало работы надд проверкой скважины через UnifocVBA

--- uniflocpy/uMultiphaseFlow/friction_Bratland.py
+++ uniflocpy/uMultiphaseFlow/friction_Bratland.py
@@ -5,8 +5,9 @@ Bratland O. Pipe flow 1: single-phase flow assurance
 //Fonte: http://www.drbratland.com/download-two-free-books-on-flow-assurance. – 2009.
 """
 import scipy.optimize as sp  # модуль для решения уравения
+import scipy.optimize._minimize as minimize
 import math
-
+import numpy as np
 
 class Friction():
     """
@@ -40,7 +41,7 @@ class Friction():
         self.f = f
         in_log10_part_first = ((1.547 / self.number_re / self.f ** (1/2))**(0.9445 * self.u_s))
         in_log10_part_second = ((self.absolute_roughness_m / 3.7 / self.d_m) ** self.u_s)
-        result = (1 / ( - 2 / self.u_s * math.log10(in_log10_part_first + in_log10_part_second)))**2
+        result = (1 / ( - 2 / self.u_s * np.log10(in_log10_part_first + in_log10_part_second)))**2
         return result - self.f
 
     def calc_f(self, number_re, epsilon_m, d_m):
@@ -56,7 +57,7 @@ class Friction():
         self.absolute_roughness_m = epsilon_m
         self.d_m = d_m
         self.relative_roughness = self.absolute_roughness_m / self.d_m
-
+        method = 'excitingmixing'
         if self.number_re <= 2300:  # Laminar
             self.f = 64 / self.number_re
         if 2300 < self.number_re <= 3100:  # Turbulent
@@ -64,12 +65,12 @@ class Friction():
             p2 = 0.04
             self.f = p1 + (p2 - p1) / (3100 - 2300) * (self.number_re - 2300)  # TODO должна быть прямая линия
         if 3100 < self.number_re <= 20000:
-            mistake = sp.fsolve(self.__correlation__, 0.02)  # TODO разобраться с выбором начального приближения
+            mistake = sp.root(self.__correlation__, 0.02, method=method).x  # TODO разобраться с выбором начального приближения
             p3 = float(self.f)
             p2 = 0.04
             self.f = p2 + (p3 - p2) / (20000 - 3100) * (self.number_re - 3100)  # TODO должна быть прямая линия
         if self.number_re > 20000:
-            mistake = sp.fsolve(self.__correlation__, 0.02)
+            mistake = sp.root(self.__correlation__, 0.02, method=method).x
         self.f = float(self.f)  # fsolve выдает numpy array, перевод в float
         return self.f
 
